c8432efd4782d347ea39fc5f8ff0622ffb0dc3d0
paisadallas <44854381+paisadallas@users.noreply.github.com>
2022-03-07 15:03:15 -0500

    Update MainActivity.kt

--- app/src/main/java/com/john/calendarevent/MainActivity.kt
+++ app/src/main/java/com/john/calendarevent/MainActivity.kt
@@ -2,11 +2,17 @@ package com.john.calendarevent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.john.calendarevent.ui.fragments.DataFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment,DataFragment.newInstance("",""))
+            .addToBackStack(null)
+            .commit()
     }
 }
